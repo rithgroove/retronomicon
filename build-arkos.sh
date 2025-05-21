affce09bf5c5512de0deeca034f8bfedcc7ee880
rithgroove <rithgroove@gmail.com>
2025-05-21 08:50:58 +0700

    add tool chain for ARkOS

---
+++ build-arkos.sh
@@ -0,0 +1,6 @@
+mkdir -p build-aarch64; 
+cd build-aarch64
+export LD_LIBRARY_PATH=/media/rithgroove/root/usr/lib/aarch64-linux-gnu
+cmake -DCMAKE_TOOLCHAIN_FILE=../ToolChain/TC-arkos.cmake \
+      -DMyGen_DIR=. ..
+make
