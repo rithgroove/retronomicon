8069258fb990512cc3cda59c4a68bdd577c8b58e
rithgroove <rithgroove@gmail.com>
2025-05-25 08:04:02 +0700

    add font

---
+++ include/retronomicon/lib/graphic/font.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <SDL_ttf.h>
+#include <string>
+
+namespace retronomicon {
+    namespace lib{
+        namespace graphic{
+
+			class Font {
+				public:
+				    Font(const std::string& filePath, int fontSize);
+				    ~Font();
+
+				    TTF_Font* getRawFont() const;
+
+				private:
+				    TTF_Font* m_font;
+			};
+        }
+    }
+} // namespace Retronomicon
---
+++ src/lib/graphic/font.cpp
@@ -0,0 +1,31 @@
+#include "retronomicon/lib/graphic/font.h"
+#include <iostream>
+
+namespace retronomicon {
+    namespace lib{
+        namespace graphic{
+            Font::Font(const std::string& filePath, int fontSize) : m_font(nullptr) {
+                if (TTF_Init() == -1) {
+                    std::cerr << "TTF_Init failed: " << TTF_GetError() << std::endl;
+                    return;
+                }
+
+                m_font = TTF_OpenFont(filePath.c_str(), fontSize);
+                if (!m_font) {
+                    std::cerr << "Failed to load font: " << filePath << ", Error: " << TTF_GetError() << std::endl;
+                }
+            }
+
+            Font::~Font() {
+                if (m_font) {
+                    TTF_CloseFont(m_font);
+                }
+                TTF_Quit();
+            }
+
+            TTF_Font* Font::getRawFont() const {
+                return m_font;
+            }
+        }
+    }
+}
