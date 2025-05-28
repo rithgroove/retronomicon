b4065324ae25dcd82b80596c29f7b77c90f96446
rithgroove <rithgroove@gmail.com>
2025-05-28 08:29:45 +0700

    update to include sdl image

--- init.sh
+++ init.sh
@@ -13,6 +13,14 @@ git clone -b release-2.24.x git@github.com:libsdl-org/SDL_ttf.git SDL2_ttf
 cd ./SDL2_ttf
 cd ..
 
+
+echo cloning SDL2_image
+git clone -b release-2.8.x git@github.com:libsdl-org/SDL_image.git SDL2_image
+cd ./SDL2_image
+cd ..
+
+
+
 # if you get sdl2 init error: no display detected, try installing these:
 #echo installing video depencencies for SDL2
 sudo apt-get install xorg-dev
