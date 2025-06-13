c1ad0f926016d56e30f2681991bf5f6060a0db6a
rithgroove <rithgroove@gmail.com>
2025-06-13 10:39:42 +0700

    finish documentation on rectangle

--- include/retronomicon/lib/core/rect.h
+++ include/retronomicon/lib/core/rect.h
@@ -45,17 +45,82 @@ namespace retronomicon::lib::core{
 	    	* @param width the width this rectangle
 	    	* @param height the height this rectangle
 	    	*/
-	    	Rect(Point* position, Point* anchor, int width, int height); //constructor position, anchor point, width, and height
-	    	// ~Point(); // no need for destructor since all attributes are native
-	    	void setSize(int width, int height);//render function (might change in the future to include renderer)
+	    	Rect(Point* position, Point* anchor, int width, int height); 
+
+
+	    	// ~Rect(); // to do : dev later
+	    	/**
+	    	 * @brief a method to change the size of the rectangle
+	    	 * 
+	    	* @param width the new width for this rectangle
+	    	* @param height the new height for this rectangle
+	    	 */
+	    	void setSize(int width, int height);
+
+	    	/**
+	    	 * @brief a method to change the position of the rectangle
+	    	 * 
+	    	* @param x the new x position for this rectangle
+	    	* @param y the new y position for this rectangle
+	    	 */
 	    	void setPosition(int x, int y);
+
+	    	/**
+	    	 * @brief a method to change the position of the rectangle
+	    	 * 
+	    	* @param position the new position in Point class
+	    	 */
 	    	void setPosition(Point* position);
+
+	    	/**
+	    	 * @brief a method to get the position of the rectangle
+	    	 * 
+	    	 * @return the position in point format
+	    	 */
 	    	Point* getPosition() const; 
+
+	    	/**
+	    	 * @brief a method to get the anchor position of the rectangle
+	    	 * 
+	    	 * @return the anchor position in point format
+	    	 */
 	    	Point* getAnchor() const;
+
+	    	/**
+	    	 * @brief a method to get the width of rectangle
+	    	 * 
+	    	 * @return the width of the rectangle
+	    	 */
 	    	int getWidth() const;
+
+	    	/**
+	    	 * @brief a method to get the height of rectangle
+	    	 * 
+	    	 * @return the height of the rectangle
+	    	 */
 	    	int getHeight() const;
+
+	    	/**
+	    	 * @brief a method to get the x position of this rectangle
+	    	 * 
+	    	 * @return the x position of the rectangle
+	    	 */
 	    	int getX() const;
+
+	    	/**
+	    	 * @brief a method to get the y position of this rectangle
+	    	 * 
+	    	 * @return the y position of the rectangle
+	    	 */
 	    	int getY() const;
+
+
+	    	/**
+	    	 * @brief a method to get SDL_Rect representation of this rectangle.
+	    	 * Useful for rendering textures.
+	    	 * 
+	    	 * @return the SDL_Rect
+	    	 */
 	    	SDL_Rect generateSDLRect();
 
 	    private:
