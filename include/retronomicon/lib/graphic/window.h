322c3cf4fd39a104e2190094b3d666a2671f6965
rithgroove <rithgroove@gmail.com>
2025-06-13 17:17:56 +0700

    adding comment for window(at least the features we've used)

--- include/retronomicon/lib/graphic/window.h
+++ include/retronomicon/lib/graphic/window.h
@@ -3,20 +3,63 @@
 #include <SDL.h>
 #include <string>
 
+/**
+ * @brief The namespace for graphic classes
+ */
 namespace retronomicon::lib::graphic{
+    /**
+     * @brief The class representing a single window
+     */
     class Window {
         public:
+            /**
+             * @brief the constructor for our windows
+             * 
+             * @param title the name of the window
+             * @param width the width of the window
+             * @param height the height of the window
+             * @param fullscreen  boolean representing fullscreen
+             */
             Window(const std::string& title, int width, int height, bool fullscreen = false);
+
+            /**
+             * @brief the destructor for the windows
+             */
             ~Window();
+
 
             void clear();
             void present();
             void toggleFullscreen();
             void handleResize(int newWidth, int newHeight);
 
+            /**
+             * @brief the method to get the window width
+             * 
+             * @return the window width
+             */
             int getWidth() const;
+
+
+            /**
+             * @brief the method to get the window height
+             * 
+             * @return the window height
+             */
             int getHeight() const;
+
+            /**
+             * @brief the method to get the renderer
+             * 
+             * @return the SDL_Renderer
+             */
             SDL_Renderer* getRenderer() const;
+
+            /**
+             * @brief the method to get the SDL_Window
+             * 
+             * @return the SDL_Window
+             */
             SDL_Window* getRawWindow() const;
 
         private:
