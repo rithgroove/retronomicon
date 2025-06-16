829b37709b3e69d53831c7b313c18695a840353e
rithgroove <rithgroove@gmail.com>
2025-06-16 20:02:07 +0700

    creat transfor component (resolves #27)

---
+++ include/retronomicon/lib/core/transform_component.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "component.h"
+
+namespace retronomicon::lib::core{
+
+    class TransformComponent : public Component {
+        public:
+            TransformComponent();
+            TransformComponent(float x, float y, float rotation = 0.0f, float scaleX = 1.0f, float scaleY = 1.0f);
+
+            void setPosition(float x, float y);
+            void setRotation(float angle);
+            void setScale(float scaleX, float scaleY);
+
+            float getX() const;
+            float getY() const;
+            float getRotation() const;
+            float getScaleX() const;
+            float getScaleY() const;
+
+        public:
+            float x;
+            float y;
+            float rotation;
+            float scaleX;
+            float scaleY;
+    };
+
+} // namespace retronomicon
---
+++ src/lib/core/transform_component.cpp
@@ -0,0 +1,31 @@
+#include "retronomicon/lib/core/transform_component.h"
+
+namespace retronomicon::lib::core{
+
+    TransformComponent::TransformComponent()
+        : x(0), y(0), rotation(0), scaleX(1.0f), scaleY(1.0f) {}
+
+    TransformComponent::TransformComponent(float x, float y, float rotation, float scaleX, float scaleY)
+        : x(x), y(y), rotation(rotation), scaleX(scaleX), scaleY(scaleY) {}
+
+    void TransformComponent::setPosition(float newX, float newY) {
+        x = newX;
+        y = newY;
+    }
+
+    void TransformComponent::setRotation(float angle) {
+        rotation = angle;
+    }
+
+    void TransformComponent::setScale(float sX, float sY) {
+        scaleX = sX;
+        scaleY = sY;
+    }
+
+    float TransformComponent::getX() const { return x; }
+    float TransformComponent::getY() const { return y; }
+    float TransformComponent::getRotation() const { return rotation; }
+    float TransformComponent::getScaleX() const { return scaleX; }
+    float TransformComponent::getScaleY() const { return scaleY; }
+
+} // namespace retronomicon
