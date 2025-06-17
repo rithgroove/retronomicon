8dae07f0395941a00c5b1024ebfaad34d98176bf
rithgroove <rithgroove@gmail.com>
2025-06-17 19:00:27 +0700

    add some comment

--- src/lib/core/game_object.cpp
+++ src/lib/core/game_object.cpp
@@ -9,18 +9,21 @@ namespace retronomicon::lib::core{
     GameObject::~GameObject() {}
 
     void GameObject::start() {
+        //loop all components and call start
         for (auto& pair : components) {
             pair.second->start();
         }
     }
 
     void GameObject::update(float dt) {
+        //loop all components and call update        
         for (auto& pair : components) {
             pair.second->update(dt);
         }
     }
 
     void GameObject::render() {
+        //loop all components and call render
         for (auto& pair : components) {
             pair.second->render();
         }
