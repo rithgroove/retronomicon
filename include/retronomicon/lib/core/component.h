df922121a88b174c0b83f87edc22b6370c863267
rithgroove <rithgroove@gmail.com>
2025-06-17 09:36:21 +0700

    add documentation to component.h

--- include/retronomicon/lib/core/component.h
+++ include/retronomicon/lib/core/component.h
@@ -6,16 +6,51 @@ class GameObject; // Forward declaration
  * @brief The namespace for basic libraries such as points, rectangle cirle, etc.
  */
 namespace retronomicon::lib::core{
+    /**
+     * @brief The interface for components (so we could easily includes multiple tipes in game objects)
+     */
     class Component {
-    public:
+       public:
+
+        /**
+         * @brief empty constructor 
+         */
         Component() = default;
+
+        /**
+         * @brief virtual destructor, if not defined use default;
+         */
         virtual ~Component() = default;
 
+        /**
+         * @brief start function (used to initialize stuff)
+         */
         virtual void start() {}
+
+        /**
+         * @brief method to update the component
+         * 
+         * @param dt time interval since last update
+         */
         virtual void update(float dt) {}
+
+        /**
+         * @brief a method to render the component if any
+         */
         virtual void render() {}
 
+        /**
+         * @brief a to set the owner of this component
+         * 
+         * @param owner the game object that owns this.
+         */
         void setOwner(GameObject* owner) { this->owner = owner; }
+
+        /**
+         * @brief a method to get the owner of this component
+         * 
+         * @return the game object that owns this component.
+         */
         GameObject* getOwner() const { return owner; }
 
     protected:
