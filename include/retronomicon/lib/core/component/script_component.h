f7a8ebb4e5705eac48d9fd73542747c2b8bbccd5
rithgroove <rithgroove@gmail.com>
2025-07-03 07:48:19 +0700

    create IScriptComponent and ScriptComponent. resolves #87

--- include/retronomicon/lib/core/component/script_component.h
+++ include/retronomicon/lib/core/component/script_component.h
@@ -1,9 +1,38 @@
-enum class ScriptBackend {
-    Lua,
-    Python
-};
+#pragma once
 
-struct ScriptComponent {
-    ScriptBackend backend;
-    // std::string scriptPath;
-};
+#include "iscript_component.h"
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+namespace retronomicon::lib::core::component {
+
+    class ScriptComponent : public IScriptComponent {
+    public:
+        ScriptComponent();
+        ScriptComponent(string path, ScriptLanguage lang);
+
+        void onStart() override;
+        void onUpdate(float deltaTime) override;
+        void onEvent(const string& eventName) override;
+
+        string getScriptPath() const override;
+        ScriptLanguage getLanguage() const override;
+
+        void setEntryFunction(string func);
+        const string& getEntryFunction() const;
+
+        bool isEnabled() const;
+        void setEnabled(bool value);
+
+        unordered_map<string, string>& getMetadata();
+
+    private:
+        string scriptPath;
+        ScriptLanguage language;
+        string entryFunction;
+        unordered_map<string, string> metadata;
+        bool enabled;
+    };
+
+} // namespace retronomicon::lib::scripting
