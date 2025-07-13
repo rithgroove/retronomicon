66f33240af4aec5b5577dbf42c6cd067b3d26854
rithgroove <rithgroove@gmail.com>
2025-07-13 19:24:47 +0700

    clean up physics system

--- src/lib/physics/physics_system.cpp
+++ src/lib/physics/physics_system.cpp
@@ -1,14 +1,14 @@
 #include "retronomicon/lib/animation/physics_system.h"
 #include "retronomicon/lib/animation/physics_component.h"
 namespace retronomicon::lib::physics{
-    AnimationSystem::AnimationSystem() {}
+    AnimationSystem::PhysicsSystem() {}
 
     void AnimationSystem::update(float dt, vector<retronomicon::lib::core::GameObject*>& objects) {
         for (GameObject* obj : objects) {
             // render logic
-            auto animation = obj->getComponent<AnimationComponent>();
-            if (!animation) continue;
-            animation->update(dt);
+            auto physicsComponent = obj->getComponent<PhysicsComponent>();
+            if (!physicsComponent) continue;
+            physicsComponent->update(dt);
         }
     }
 }
