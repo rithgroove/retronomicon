14e734d1e460b71eea903ac2a63016db27c33400
rithgroove <rithgroove@gmail.com>
2025-07-23 07:31:11 +0700

    restore scene.h

--- include/retronomicon/lib/core/scene.h
+++ include/retronomicon/lib/core/scene.h
@@ -6,44 +6,29 @@
 #include "retronomicon/lib/core/entity.h"
 #include "system.h"
 
+using namespace std;
 namespace retronomicon::lib::core {
 
     class Scene : public Entity {
     public:
-        explicit Scene(const std::string& name);
-        virtual ~Scene();
+        
+        Scene(const std::string& name);
+        ~Scene();
 
-        // Lifecycle
-        virtual void init();
-        virtual void update(float dt);
-        virtual void render();
-        virtual void shutdown();
-        virtual void reset();
+        void init();
+        void update(float dt);
+        void render();
+        void shutdown();
 
-        // GameObject & System management
-        Entity* createGameObject(const std::string& name);
+        string getName() const;
+
+        Entity* createGameObject(const string& name);
         void removeGameObject(Entity* object);
         void addSystem(System* system);
 
-        // State accessors (inlined)
-        inline const std::string& getName() const { return m_name; }
-        inline bool isInitialized() const { return m_isInitialized; }
-        inline bool isActive() const { return m_isActive; }
-        inline bool requiresReset() const { return m_requiresReset; }A
-
-        inline void setInitialized(bool value) { m_isInitialized = value; }
-        inline void setActive(bool value) { m_isActive = value; }
-        inline void setRequiresReset(bool value) { m_requiresReset = value; }
-
-    protected:
-        std::string m_name;
-
-        std::vector<Entity*> m_gameObjects;
-        std::vector<System*> m_systems;
-
-        bool m_isInitialized = false;
-        bool m_isActive = false;
-        bool m_requiresReset = false;
+    private:
+        string m_name;
+        vector<Entity*> m_gameObjects;
+        vector<System*> m_systems;
     };
-
 }
