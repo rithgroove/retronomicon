d8ea55e2cae73bbf85def064e5357a4a6573751b
rithgroove <rithgroove@gmail.com>
2025-09-16 10:12:40 +0700

    refactor renderable

--- include/retronomicon/core/ecs/renderable.h
+++ include/retronomicon/core/ecs/renderable.h
@@ -1,22 +1,28 @@
 #pragma once
 
-#include <SDL.h>
 #include "retronomicon/lib/math/rect.h"
-namespace retronomicon::lib::core {
+/**
+ * @brief The namespace for ECS libraries that will be the building blocks for the engine
+ */
+namespace retronomicon::core::ecs{
     using retronomicon::lib::math::Rect;
     /**
-     * @brief Optional interface for components that render with SDL.
+     * @brief Optional interface for components that can be rendered.
      */
     class Renderable {
-    public:
-        virtual ~Renderable() = default;
+        public:
 
-        /**
-         * @brief Called when rendering with SDL
-         */
-        virtual void render(SDL_Renderer* renderer) = 0;
+            /**
+             * @brief Called when rendering with SDL
+             */
+            virtual void render() = 0;
 
-        virtual Rect getSize() = 0;
+            /**
+             * @brief method to calculate the size of the rendered component
+             * 
+             * @return size in rectangle format
+             */
+            virtual Rect getSize() = 0;
     };
 
-} // namespace retronomicon::lib::core
+} // namespace retronomicon::core::ecs
