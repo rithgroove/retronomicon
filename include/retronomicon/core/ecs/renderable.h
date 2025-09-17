8698806dfe6ab9366eded604a570263615d7dad9
rithgroove <rithgroove@gmail.com>
2025-09-17 08:13:08 +0700

    remove rect from renderable because we're going to use bound component later.also make renderable extends component

--- include/retronomicon/core/ecs/renderable.h
+++ include/retronomicon/core/ecs/renderable.h
@@ -1,28 +1,19 @@
 #pragma once
 
-#include "retronomicon/lib/math/rect.h"
 /**
  * @brief The namespace for ECS libraries that will be the building blocks for the engine
  */
 namespace retronomicon::core::ecs{
-    using retronomicon::lib::math::Rect;
     /**
      * @brief Optional interface for components that can be rendered.
      */
-    class Renderable {
+    class Renderable  : public Component {
         public:
 
             /**
              * @brief Called when rendering with SDL
              */
             virtual void render() = 0;
-
-            /**
-             * @brief method to calculate the size of the rendered component
-             * 
-             * @return size in rectangle format
-             */
-            virtual Rect getSize() = 0;
     };
 
 } // namespace retronomicon::core::ecs
