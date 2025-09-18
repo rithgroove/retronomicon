727b9a6f66325414d3176a99f92619702a1133b3
rithgroove <rithgroove@gmail.com>
2025-09-18 09:43:34 +0700

    create namespace asset

---
+++ include/retronomicon/asset/asset.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <string>
+
+/**
+ * @brief The namespace for assets and loaders
+ */
+namespace retronomicon::asset {
+    /**
+     * @brief abstract class that will be extended by many asset types
+     */ 
+    class Asset {
+        public:
+
+            /***************************** Destructor *****************************/
+
+            /**
+             * @brief default destructor
+             */ 
+            virtual ~Asset() = default;
+
+            /***************************** Getter *****************************/
+
+            /**
+             * @brief getter for path
+             * 
+             * @return path to the file
+             */
+            std::string getPath() noexcept {return m_path;}
+            
+            /**
+             * @brief getter for filename
+             * 
+             * @return name of the file
+             */
+            std::string getName() noexcept {return m_name;}
+
+            /***************************** Operator Overload *****************************/
+
+            /**
+             * @brief overloading operator << to call to_string()
+             */
+            friend std::ostream& operator<<(std::ostream& os, const Asset& obj) {
+                return os << obj.to_string();
+            }
+
+            /***************************** To String *****************************/
+
+            /**
+             * @brief a method to help people debug this object
+             * 
+             * @return Brief summary of this object in string
+             */
+            virtual std::string to_string() const = 0;
+
+        protected:
+            /***************************** attributes *****************************/
+            std::string m_path;
+            std::string m_name;
+    };
+}
