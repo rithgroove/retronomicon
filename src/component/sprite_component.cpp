2eb477bf82964d5b2d6c19492916fcd78a80791c
rithgroove <rithgroove@gmail.com>
2025-11-08 09:59:35 +0700

    remove debug code

--- src/component/sprite_component.cpp
+++ src/component/sprite_component.cpp
@@ -52,18 +52,8 @@ namespace retronomicon::component {
             int h = static_cast<int>(m_image->getHeight() * m_transform->getScaleY());
             int centerX = static_cast<int>(w * m_transform->getAnchorX());
             int centerY = static_cast<int>(h * m_transform->getAnchorY());
-            std::cout << "x : " << x << std::endl;
-            std::cout << "y : " << y << std::endl;
             x-=centerX;
             y-=centerY;
-            std::cout << "x after : " << x << std::endl;
-            std::cout << "y after : " << y << std::endl;
-            std::cout << "w : " << w << std::endl;
-            std::cout << "h : " << h << std::endl;
-            std::cout << "center x : " << centerX << std::endl;
-            std::cout << "center y : " << centerY << std::endl;
-            std::cout << "scale x : " << m_transform->getScaleX() << std::endl;
-            std::cout << "scale y : " << m_transform->getScaleY() << std::endl;
         }
         renderer->render(m_texture, Vec2(float(x) ,float(y)), Vec2(m_transform->getScaleX(),m_transform->getScaleY()), 0.0f, 1.0f);
     }
